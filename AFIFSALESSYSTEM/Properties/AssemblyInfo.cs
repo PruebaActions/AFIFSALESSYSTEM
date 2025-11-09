9256f749d6b6a2e65ce604dcd5e0092d5c237660
Frank <frank.reyes@solvex.com.do>
2025-11-09 14:37:26 -0400

    asdsadsasadsad

--- AFIFSALESSYSTEM/Properties/AssemblyInfo.cs
+++ AFIFSALESSYSTEM/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("9867.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("986.0.0.0")]
